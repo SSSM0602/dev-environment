fa70529253e602318844b37fd7c81a3322202075
Saketh Medicherla <saketh.medicherla@gmail.com>
2025-04-05 17:23:55 -0400

    Added autocompletion

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -22,14 +22,7 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
 
-vim.api.nvim_create_autocmd('LspAttach', {
-  callback = function(ev)
-    local client = vim.lsp.get_client_by_id(ev.data.client_id)
-    if client:supports_method('textDocument/completion') then
-      vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
-    end
-  end,
-})
+
 
 -- Setup lazy.nvim
 require("lazy").setup({
@@ -122,15 +115,18 @@ require("lazy").setup({
         },
         {
             "williamboman/mason-lspconfig.nvim",
-            dependencies = {"mason.nvim"}, 
+            dependencies = {"mason.nvim", "saghen/blink.cmp"}, 
             config = function() 
+
+                local capabilities = require('blink.cmp').get_lsp_capabilities()
                 require("mason-lspconfig").setup()
                 require("mason-lspconfig").setup_handlers({
                     -- The first entry (without a key) will be the default handler
                     -- and will be called for each installed server that doesn't have
                     -- a dedicated handler.
                     function (server_name) -- default handler (optional)
-                        require("lspconfig")[server_name].setup({})
+                        require("lspconfig")[server_name].setup({capabilities = capabilities})
+                        
                     end,
                     -- Next, you can provide a dedicated handler for specific servers.
                     -- For example, a handler override for the `rust_analyzer`:
@@ -156,6 +152,48 @@ require("lazy").setup({
                     }
                 end)
             end
+        },
+        {
+            'saghen/blink.cmp',
+            -- optional: provides snippets for the snippet source
+            dependencies = { 'rafamadriz/friendly-snippets' },
+
+            -- use a release tag to download pre-built binaries
+            version = '1.*',
+            -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+            -- build = 'cargo build --release',
+            -- If you use nix, you can build from source using latest nightly rust with:
+            -- build = 'nix run .#build-plugin',
+
+            ---@module 'blink.cmp'
+            ---@type blink.cmp.Config
+            opts = {
+                keymap = { preset = 'default' },
+
+                appearance = {
+                    -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+                    -- Adjusts spacing to ensure icons are aligned
+                    nerd_font_variant = 'mono',
+                    use_nvim_cmp_as_default = true
+                },
+
+                -- (Default) Only show the documentation popup when manually triggered
+                completion = { documentation = { auto_show = false } },
+
+                -- Default list of enabled providers defined so that you can extend it
+                -- elsewhere in your config, without redefining it, due to `opts_extend`
+                sources = {
+                    default = { 'lsp', 'path', 'snippets', 'buffer' },
+                },
+
+                -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+                -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+                -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+                --
+                -- See the fuzzy documentation for more information
+                fuzzy = { implementation = "prefer_rust_with_warning" }
+            },
+            opts_extend = { "sources.default" }
         }
     },
     -- Configure any other settings here. See the documentation for more details.
